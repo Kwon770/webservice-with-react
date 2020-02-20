e8ee0fe54f0cd6a251ebdd01721c54eff27cac01
Kwon770 <sckwon770@gmail.com>
2020-02-21 07:38:53 +0900

    1 React Work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// Automatic refesh web from react
+// Generate element with react and App.js
+// and Put html in div.id=root of index.html virtually
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>Hello!</div>;
 }
 
 export default App;
