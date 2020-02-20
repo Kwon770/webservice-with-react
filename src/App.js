b20837f0aea854c8aa29f44778ddfef5db2f6de2
Kwon770 <sckwon770@gmail.com>
2020-02-21 08:03:57 +0900

    2-1 Reusable components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -7,11 +7,20 @@ import Potato from "./Potato.js";
 // Generate element with react and App.js
 // and Put html in div.id=root of index.html virtually
 
+// Food(props) : props is object. object.name
+// Food({ name }) : name is value. name
+function Food({ name }) {
+  return <h1>I also like {name}</h1>;
+}
+
 function App() {
   return (
     <div>
       <h1>Hello!</h1>
       <Potato />
+      <Food name="kimchi" isKoreanFood={true} />
+      <Food name="cheese" isKoreanFood={false} />
+      <Food name="sushi" isKoreanFood={false} />
     </div>
   );
 }
