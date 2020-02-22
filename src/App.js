c617f8a728cdc0c31e2574cd0a85a500f46c1819
Kwon770 <sckwon770@gmail.com>
2020-02-23 08:47:56 +0900

    4-3 Adding genres

--- src/App.js
+++ src/App.js
@@ -41,13 +41,13 @@ class App extends React.Component {
 
     // Deliever Movie parameter data, Get Movie HTML
     return (
-      <section class="container">
+      <section className="container">
         {isLoading ? (
-          <div class="loader">
-            <span class="loader__text">Loading...</span>
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
           </div>
         ) : (
-          <div class="movies">
+          <div className="movies">
             {movies.map(movie => (
               <Movie
                 key={movie.id}
@@ -56,6 +56,7 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
             ))}
           </div>
--- src/Movie.js
+++ src/Movie.js
@@ -2,15 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-// If component don't need state, just Use function, not class
-function Movie({ id, year, title, summary, poster }) {
+// If component donxw't need state, just Use function, not classx
+// Html in function or component is JSX, thus we must use className, not class
+// Map give two data, item and itemNumber => You can use itemNumber as key to prevent error
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <div class="movie">
+    <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <div class="movie__data">
-        <h3 class="movie__title">{title}</h3>
-        <h5 calss="movie__year">{year}</h5>
-        <p class="movie__summary">{summary}</p>
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 calssName="movie__year">{year}</h5>
+        <ul calssName="genres">
+          {genres.map((genre, index) => (
+            <li key={index} calssName="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary}</p>
       </div>
     </div>
   );
@@ -21,7 +30,8 @@ Movie.prototype = {
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
