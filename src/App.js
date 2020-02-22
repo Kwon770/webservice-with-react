9bc3b5a2976443e7df181bfa508b3a8a7b624130
Kwon770 <sckwon770@gmail.com>
2020-02-22 12:28:37 +0900

    3-1 All you need to know about state

--- src/App.js
+++ src/App.js
@@ -8,16 +8,28 @@ class App extends React.Component {
     count: 0
   };
 
-  add = () => {};
-  minus = () => {};
+  // setState(): Change date and Refresh certain tag with render()
+  // 'current' make this can apporach state
+  // this.state.DATA is bad format
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1
+    }));
+  };
+  minus = () => {
+    this.setState(current => ({
+      count: current.count - 1
+    }));
+  };
 
   // To get data of class component, Use 'this'
+  // To deliever object, Don't write '()'
   render() {
     return (
       <div>
         <h1>The number is {this.state.count} </h1>
-        <button onClick={this.add()}>Add</button>
-        <button onClick={this.minus()}>Minus</button>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
       </div>
     );
   }
