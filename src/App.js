928a2cedcb10503a8c1e96ff2e2c7823febbf5de
Kwon770 <sckwon770@gmail.com>
2020-02-25 08:53:50 +0900

    6-4 Redirecting

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Navigation></Navigation>
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
-      <Route path="/movie-detail" component={Detail} />
+      <Route path="/movie/:id" component={Detail} />
     </HashRouter>
   );
 }
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,24 @@
 import React from "react";
 
-function Detail(props) {
-  return <span>a</span>;
+class Detail extends React.Component {
+  componentDidMount() {
+    // location, history, .. etc props은 Route 으로부터 주어짐
+    const { location, history } = this.props;
+    // 클릭에 의한 것이 아닌 임의적으로 주소를 통해 들어온 경우 전달받은 props이 없음
+    // 이 경우 Component 가 정상작동 하지 않으므로, 홈으로 Redirectinfg
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  render() {
+    const { location } = this.props;
+    // render() 가 componentDidMount() 보다 빠르므로 예외 방지
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
+  }
 }
 
 export default Detail;
