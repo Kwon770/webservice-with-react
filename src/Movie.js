2b8e3e83b558468da131b3ba0fcbfb018570a789
Kwon770 <sckwon770@gmail.com>
2020-02-24 15:14:18 +0900

    4-5 Cutting the summary

--- src/Movie.js
+++ src/Movie.js
@@ -25,7 +25,7 @@ function Movie({ id, year, title, summary, poster, genres }) {
             </li>
           ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 140)}...</p>
       </div>
     </div>
   );
