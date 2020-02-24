3706d1069020864458cb6f79e3fc1118ccb03772
Kwon770 <sckwon770@gmail.com>
2020-02-24 16:46:43 +0900

    6-1 Building the Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
 function App() {
-  return <span>semething</span>;
+  // path: "https://<URL>/<HASH>"" 이후 부분의 url
+  // path="/about" => "https://<URL>/<HASH>/about"
+
+  // exact={true} 일 경우, 정확하게 일치하는 path에서만 랜더링
+
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { func } from "prop-types";
+
+function About() {
+  return <span>About this page</span>;
+}
+
+export default About;
